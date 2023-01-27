5179c4b6c9610de5ccd49b6e5710f90a89ba6e97
OFFICEKIEV\y.tishchenko <y.tishchenko@temabit.com>
2023-01-27 05:04:24 +0200

    Add GameModel and and its to Imports

---
+++ GameIndastry_v2_0/GameIndastry_v2_0/Models/GameModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace GameIndastry_v2_0.Models
+{
+	public class GameModel
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Genre { get; set; }
+		public DateTime ReleaseDate { get; set; }
+
+		public string Img { get; set; }
+	}
+}
